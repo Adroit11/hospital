a2c057be5101acb5f71598b8b685299a2c6c56d8
Masiba <dominiqueadriano@gmail.com>
2015-01-29 15:33:11 +0300

    Revert "form label"
    This reverts commit a23c1e4902e8caee9c877453d6ba1e7f97141712.

--- app/views/item/create.blade.php
+++ app/views/item/create.blade.php
@@ -14,57 +14,57 @@
 
     <!-- Complain-->
     <div class="form-group">
-        {{ Form::label('type', 'Item Type', array('class' => 'col-sm-2 control-label')) }}
+        {{ Form::label('type', 'type', array('class' => 'col-sm-2 control-label')) }}
         <div class="col-sm-10">
-            {{ Form::text('type', null, array('class' => 'form-control', 'placeholder'=>'Item Type')) }}
+            {{ Form::text('type', null, array('class' => 'form-control', 'placeholder'=>'Sponser Name')) }}
         </div>
     </div>
 
     <!-- code-->
     <div class="form-group">
-        {{ Form::label('code', 'Item Code', array('class' => 'col-sm-2 control-label')) }}
+        {{ Form::label('code', 'code', array('class' => 'col-sm-2 control-label')) }}
         <div class="col-sm-10">
-            {{ Form::text('code', null, array('class' => 'form-control', 'placeholder'=>'Code')) }}
+            {{ Form::text('code', null, array('class' => 'form-control', 'placeholder'=>'Bill Type')) }}
         </div>
     </div>
 
     <!-- Comment-->
     <div class="form-group">
-        {{ Form::label('unit', 'Unit', array('class' => 'col-sm-2 control-label')) }}
+        {{ Form::label('unit', 'unit', array('class' => 'col-sm-2 control-label')) }}
         <div class="col-sm-10">
-            {{ Form::text('unit', null, array('class' => 'form-control', 'placeholder'=>'Item Unit')) }}
+            {{ Form::text('unit', null, array('class' => 'form-control', 'placeholder'=>'Transaction Status')) }}
         </div>
     </div>
 
     <!-- lab-->
     <div class="form-group">
-        {{ Form::label('name', 'Item Name', array('class' => 'col-sm-2 control-label')) }}
+        {{ Form::label('name', 'name', array('class' => 'col-sm-2 control-label')) }}
         <div class="col-sm-10">
-            {{ Form::text('name', null, array('class' => 'form-control', 'placeholder'=>'Item Name')) }}
+            {{ Form::text('name', null, array('class' => 'form-control', 'placeholder'=>'Region')) }}
         </div>
     </div>
 
     <!-- code-->
     <div class="form-group">
-        {{ Form::label('price', 'Price', array('class' => 'col-sm-2 control-label')) }}
+        {{ Form::label('price', 'price', array('class' => 'col-sm-2 control-label')) }}
         <div class="col-sm-10">
-            {{ Form::text('price', null, array('class' => 'form-control', 'placeholder'=>'Price')) }}
+            {{ Form::text('price', null, array('class' => 'form-control', 'placeholder'=>'Bill Type')) }}
         </div>
     </div>
 
     <!-- Comment-->
     <div class="form-group">
-        {{ Form::label('status', 'Status', array('class' => 'col-sm-2 control-label')) }}
+        {{ Form::label('status', 'status', array('class' => 'col-sm-2 control-label')) }}
         <div class="col-sm-10">
-            {{ Form::text('status', null, array('class' => 'form-control', 'placeholder'=>'Status')) }}
+            {{ Form::text('status', null, array('class' => 'form-control', 'placeholder'=>'Transaction Status')) }}
         </div>
     </div>
 
     <!-- lab-->
     <div class="form-group">
-        {{ Form::label('con_type', 'Constration', array('class' => 'col-sm-2 control-label')) }}
+        {{ Form::label('con_type', 'con_type', array('class' => 'col-sm-2 control-label')) }}
         <div class="col-sm-10">
-            {{ Form::text('con_type', null, array('class' => 'form-control', 'placeholder'=>'Constration')) }}
+            {{ Form::text('con_type', null, array('class' => 'form-control', 'placeholder'=>'Region')) }}
         </div>
     </div>
 
