3340a1dc2fa64904729c4c5563d11ae9b60f4824
Gabby <gabbykweka@gmail.com>
2015-01-29 14:19:08 +0300

    cont5

--- app/controllers/LogController.php
+++ app/controllers/LogController.php
@@ -6,6 +6,36 @@
  * Time: 1:40 PM
  */
 
-class LogController {
+class LogController extends BaseController {
+
+    public function getLogin (){
+        return View::make('login');
+
+    }
+
+    public function postLogin (){
+
+        if (Auth::attempt(array('name'=>Input::get('name'), 'password'=>Input::get('password')))) {
+            $name = Auth::user()->name;
+            $level = Auth::user()->level;
+            Session::put('myName', $name);
+            Session::put('myLevel',$level);
+            return Redirect::to('dashboard')->with('message', 'You are now logged in!');
+        } else {
+            return Redirect::back()
+                ->with('error', 'Your username/password combination was incorrect!')
+                ->withInput();
+        }
+    }
+
+    public function getLogout()
+    {
+        Auth::logout(); // log the user out of our application
+        Session::forget('myName');
+        Session::forget('myLevel');
+        return Redirect::to('login')
+            ->with('message', 'You are now logged Out...!!'); // redirect the user to the login screen
+    }
+
 
 } 
---
+++ app/views/login.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.log')
+@section('content')
+
+<!-- if there are creation errors, they will show here -->
+@include('includes.message')
+
+<ul>
+    @foreach($errors->all() as $error)
+    <li>{{ $error }}</li>
+    @endforeach
+</ul>
+{{ Form::open(array('url'=>'login', 'class'=>'form-signin','role'=>'form')) }}
+<h2 class="form-signin-heading">Please Log in</h2>
+<!-- Name-->
+<div class="form-group">
+    {{ Form::label('name', 'Name', array('class' => 'sr-only control-label')) }}
+    <div>
+        {{ Form::text('name', null, array('class' => 'form-control', 'placeholder'=>'Name')) }}
+    </div>
+</div>
+
+<!-- Password-->
+<div class="form-group">
+    {{ Form::label('password', 'Password', array('class' => 'sr-only control-label')) }}
+    <div>
+        {{ Form::password('password', array('class' => 'form-control', 'placeholder'=>'Password')) }}
+    </div>
+</div>
+
+<!-- Button (Double) -->
+<div class="form-group">
+    <div>
+        <button type="submit"  class="btn btn-primary btn-block btn-lg">Log In</button>
+    </div>
+</div>
+{{ Form::close() }}
+
+@stop
